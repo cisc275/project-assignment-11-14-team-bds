5dc8b3aece65194b76f6db035d2e8f1a7c3b73c1
Dodobirds <01010011c@gmail.com>
2019-04-15 12:28:45 -0400

    patths kinda work

--- src/entities/Path.java
+++ src/entities/Path.java
@@ -1,4 +1,60 @@
 package entities;
-public class Path extends Enemy {
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Path implements Collidable {
+
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+	
+	public Path(int x, int y, int w, int h) {
+		xPos = x;
+		yPos = y;
+		width = w;
+		height = h;
+	}
+	@Override
+	public boolean collideWith(Collidable that) {
+		return false;
+	}
+
+	@Override
+	public void onCollide(Collidable that) {
+	}
+
+	@Override
+	public void render(Graphics g) {
+		g.setColor(new Color(0,0,255,20));
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
+
+	@Override
+	public void update() {
+		yPos += 10;
+		
+	}
+
+	@Override
+	public int getX() {
+		return xPos;
+	}
+
+	@Override
+	public int getY() {
+		return yPos;
+	}
+
+	@Override
+	public int getWidth() {
+		return width;
+	}
+
+	@Override
+	public int getHeight() {
+		return height;
+	}
 
 }
--- src/models/OspreyModel.java
+++ src/models/OspreyModel.java
@@ -6,10 +6,13 @@ import entities.Bird;
 import entities.Collidable;
 import entities.Enemy;
 import entities.Osprey;
+import entities.Path;
 
 public class OspreyModel extends Model {
 	private Bird player = new Osprey();
-	private List<Collidable> entities = new ArrayList<>() ;
+	private List<Collidable> entities = new ArrayList<>();
+	
+	private Path lastPath = new Path(300, 0, 100, 20);
 	
 	public OspreyModel() {
 		super();
@@ -49,7 +52,7 @@ public class OspreyModel extends Model {
 				}
 			}
 		}
-		spawnEnemy();
+		spawnPath(lastPath);
 	}
 	@Override
 	public List<Collidable> getEntities() {return entities;};
@@ -65,5 +68,13 @@ public class OspreyModel extends Model {
 		if (a == 0) {
 			entities.add(new Enemy(r.nextInt(400), 0, 125, 100));
 		}
+	}
+	
+	private void spawnPath(Collidable prev) {
+		Random r = new Random();
+		int offset = r.nextInt(100) - 50;
+		Path p = new Path(prev.getX() + offset, 0, 150, 10);
+		entities.add(p);
+		lastPath = p;
 	}
 }
