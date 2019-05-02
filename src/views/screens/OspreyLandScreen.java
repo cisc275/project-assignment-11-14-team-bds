6190216b4bca5eb290fa57b5629d607e14436d1a
Sean <snemes@udel.edu>
2019-05-02 15:05:45 -0400

    Simple scrolling background for Osprey game

--- src/views/screens/OspreyLandScreen.java
+++ src/views/screens/OspreyLandScreen.java
@@ -1,19 +1,27 @@
 package views.screens;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import entities.Collidable;
+import views.ImageHandler;
 import views.View;
 
 public class OspreyLandScreen extends View{
 	
 	List<Collidable> entities = new ArrayList<>();
 	
+	private int backgroundScrollAmount = 0;
+	private final String BG_IMAGE_PATH = "res/osprey_bg.png";
+	private BufferedImage backgroundImage;
+	private int backgroundScrollSpeed = 2;
+	
 	public OspreyLandScreen(JFrame f) {
+		backgroundImage = ImageHandler.loadImage(BG_IMAGE_PATH);
 		frame = f;
 		setScreen(frame);
 	}
@@ -39,6 +47,18 @@ public class OspreyLandScreen extends View{
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponents(g);
+				//draw background first
+				//draw it twice, one above and one below
+				if(backgroundScrollAmount < FRAME_HEIGHT) { //probably not the best place for this to be
+					backgroundScrollAmount += backgroundScrollSpeed;
+				} else {
+					backgroundScrollAmount = 0;
+				}
+				
+				g.drawImage(backgroundImage, 0, backgroundScrollAmount-FRAME_HEIGHT, FRAME_WIDTH, FRAME_HEIGHT, null);
+				g.drawImage(backgroundImage, 0, backgroundScrollAmount, FRAME_WIDTH, FRAME_HEIGHT, null);
+				
+				
 				g.setColor(new Color(255,0,0));
 				g.drawString("This is the Osprey Game", FRAME_WIDTH / 2, FRAME_HEIGHT / 2);
 				
