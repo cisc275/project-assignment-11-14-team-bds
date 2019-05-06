d75d129fc6b6b322b943fe3977e286b1c558f004
Sean Nemes <snemes@udel.edu>
2019-05-06 13:43:10 -0400

    Clapper Rail image

--- src/entities/ClapperRail.java
+++ src/entities/ClapperRail.java
@@ -1,7 +1,23 @@
 package entities;
+
+import java.awt.Graphics;
+
 public class ClapperRail extends Bird{
 	private int branches;
 	private boolean invisible;
+	private Sprite sprite;
+	
+	
+	public ClapperRail() {
+		sprite = new Sprite(1, "res/clapper-rail.png");
+	}
+	
+	@Override
+    public void render(Graphics g) {
+		int top = this.xPos - (this.width /2);
+		int left = this.yPos - (this.height /2 );
+		g.drawImage(sprite.getCurrentFrame(), top, left, this.width, this.height, null);
+    }
 	
 	public int getNumBranches() {
 		return this.branches;
