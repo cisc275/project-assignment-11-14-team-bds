84cf84c0d6fb8e295960476bfb3936435d432ccf
Jon Hajrizi <jhajrizi@wifi-roaming-128-4-155-187.host.udel.edu>
2019-05-13 17:14:40 -0400

    add javadocs

--- src/entities/Bird.java
+++ src/entities/Bird.java
@@ -23,20 +23,37 @@ public class Bird extends Collidable {
 	private float speedMultiplier = 1f;
 	
 	public Bird(){
+		// TODO: fix magic numbers
 		super(320, 400, 30,30);
 		this.health = 100;
 	}
+
+	/**
+	 @desc sets the velocity along the x vector
+	 @param int x - velocity value
+	 @return void
+	 */
 	public void setXVelocity(int x) {
 		this.xDir = x;
 	}
+	/**
+	 @desc sets the velocity along the y vector
+	 @param int y - velocity value
+	 @return void
+	 */
 	public void setYVelocity(int y) {
 		this.yDir = y;
 	}
 	
+	/**
+	 @desc sets the value of hidden if the bird is hidden in gamee
+	 @param booleean h - status of hidden
+	 @return void
+	 */
 	public void setHidden(boolean h) {
 		hidden = h;
 	}
-	
+	// TODO: duplicate methods?
 	public void setYVelocity() {
 		this.yDir = 2;
 	}
@@ -44,36 +61,71 @@ public class Bird extends Collidable {
 		this.xDir = 0;
 	}
 	
+	/**
+	 @desc increments counter variable by 1
+	 @return void
+	 */
 	public void incCount() {
-		counter = counter + 1;
+		counter = counter++;
 	}
 	
+	/**
+	 @desc decrements the value of counter
+	 @param int dec - amount to decrement by
+	 @return void
+	 */
 	public void decCount(int dec) {
 		counter = counter - dec;
+		// TODO: magic variables
 		if (counter < 0)
 			counter = 0;
 	}
 	
+	/**
+	 @desc gets the current value of counter
+	 @return int counter - value of counter
+	 */
 	public int getCount() {
 		return counter;
 	}
 	
+	/**
+	 @desc used to retrieve value of hidden
+	 @return boolean hidden - true if bird is hidden false if not
+	 */
 	public boolean isHidden() {
 		return hidden;
 	}
 	
+	/**
+	@desc returns value of diving if the bird is in diving state
+	@return boolean diving - true if bird is diving false otherwise
+	 */
 	public boolean isDiving() {
 		return diving;
 	}
 
+	/**
+	 @desc sets value of diving to true when bird dives
+	 @return void
+	 */
 	public void dive() {
 		diving = true;
 	}
 	
+	/**
+	 @desc sets cvalue of diving to false when bird is no longer diving
+	 @return void
+	 */
 	public void unDive() {
 		diving = false;
 	}
 	
+	/**
+	 @desc decrements the Bird's health
+	 @param int damage - the amount of health to be decremented
+	 @return void
+	 */
 	public void decHealth(int damage) {
 		health -= damage;
 		System.out.println("Damage taken: " + damage);
@@ -82,6 +134,11 @@ public class Bird extends Collidable {
 		}
 	}
 	
+	/**
+	 @desc increments the birds health
+	 @param int damage - amount of health to be restored
+	 @return void
+	 */
 	public void incHealth(int damage) {
 		health += damage;
 		System.out.println("Damage healed: " + damage);
@@ -90,6 +147,13 @@ public class Bird extends Collidable {
 		}
 	}
 	
+	/**
+	 @desc pushes bird in a certain direction when hitting wind
+	 @param int direction - direction of gust
+	 @param int moveDis - distance to move
+	 @param int time - duration of effect
+	 @return void
+	 */
 	public void pushBird(int direction, int moveDis, int time) {
 		if (wind) {
 			return;
@@ -122,7 +186,7 @@ public class Bird extends Collidable {
 		}
 		wind  = true;
 	}
-
+	
 	public void giveTimedPowerup(BirdProperties prop, int timeActive) {
 		//add a timer to this somehow, return to default state after done
 		setProperties(prop);
--- src/models/ClapperRailModel.java
+++ src/models/ClapperRailModel.java
@@ -30,15 +30,30 @@ public class ClapperRailModel extends Model {
 		entities.add(new Grass());
 	}
 	
+	/**
+	 @desc sets the players x velocity
+	 @param int x - number of pixels to travel for x velocity
+	 @return void
+	 */
 	@Override
 	public void setPlayerX(int x) {
 		player.setXVelocity(x);
 	}
-	
+
+	/**
+	 @desc sets the players y velocity
+	 @param int y - number of pixels to travel for y velocity
+	 @return void
+	 */
 	@Override
 	public void setPlayerY(int x) {
 		player.setYVelocity(x);
 	}
+
+	/**
+	@desc handles collisions between game objects
+	@return void
+	 */
 	@Override
 	public void updateCollidables() {
 		System.out.println(player.getCount());
@@ -65,6 +80,10 @@ public class ClapperRailModel extends Model {
 		spawnStick();
 	}
 	
+	/**
+	 @desc handles generation of sticks
+	 @return void
+	 */
 	public void spawnStick() {
 		Random r = new Random();
 		if(r.nextInt(100) < 91 && sticksMap < 5) { //5% chance of spawn
@@ -74,10 +93,18 @@ public class ClapperRailModel extends Model {
 		}
 	}
 	
+	/**
+	 @desc decrement sticksMap value by 1
+	 @return void
+	 */
 	public void decSticksMap() {
-		sticksMap = sticksMap - 1;
+		sticksMap = sticksMap--;
 	}
 	
+	/**
+	 @desc spawns grass objects on screen
+	 @return void
+	 */
 	public void spawnGrass() {
 		entities.add(new Grass());
 	}
